116e9861161e59d3f3fc1bf91267814bdae9cf45
Dev Dibyanshu <devdibyanshu12321@gmail.com>
2026-08-04 10:06:15 +0530

    Q10 added

---
+++ TopBrains/Q10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Solution
+{
+    public static int LargestInteger(int a, int b, int c)
+    {
+        if (a >= b && a >= c)
+        {
+            return a;
+        }
+        else if (b >= a && b >= c)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(LargestInteger(10, 20, 30)); // 30
+        Console.WriteLine(LargestInteger(50, 20, 30)); // 50
+        Console.WriteLine(LargestInteger(10, 60, 30)); // 60
+    }
+}
